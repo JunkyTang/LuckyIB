595c024376c18fb4bea781fec8f222d2706b4a85
汤俊杰 <921969987@qq.com>
2024-10-12 17:25:10 +0800

    1.3.0 新增stackView

---
+++ IBStackView.swift
@@ -0,0 +1,56 @@
+//
+//  IBStackView.swift
+//  LuckyIB
+//
+//  Created by junky on 2024/10/12.
+//
+
+import UIKit
+
+
+class IBStackView: UIStackView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    @IBInspectable var shadowColor: UIColor = .clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = .zero {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 3 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+}
